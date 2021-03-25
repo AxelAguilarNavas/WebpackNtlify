e001ffa0200cf01b5fc6c5659b2083ef12d101f1
AxelAguilarNavas <axl_Xbox360@hotmail.com>
2021-03-24 18:52:40 -0700

    fix bug III

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -3,9 +3,9 @@ const API = process.env.API;
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
   try {
+    console.log('Data:', apiURl);
     const response = await fetch(apiURl);
     const data = await response.json();
-    console.log('Data:', data);
     return data.results[0];
   } catch (error) {
     console.log('Fetch Error', error);
