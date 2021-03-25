cac7000d571110141485b896d553a42bf5c54e40
AxelAguilarNavas <axl_Xbox360@hotmail.com>
2021-03-24 18:35:46 -0700

    fix bug II

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -5,6 +5,7 @@ const getData = async (id) => {
   try {
     const response = await fetch(apiURl);
     const data = await response.json();
+    console.log('Data:', data);
     return data.results[0];
   } catch (error) {
     console.log('Fetch Error', error);
